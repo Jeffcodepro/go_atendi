186b8dd42057c4dd70d7380485e739311622d8d7
Jefferson Santos <jeffersonoliveira1212@gmail.com>
2026-07-02 11:37:50 -0300

    Ajustando footer com informações de termo de uso e outras informações necessárias

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root "pages#home"
 
+  get "privacidade", to: "pages#privacidade", as: :privacidade
+  get "termos", to: "pages#termos", as: :termos
+  get "cookies", to: "pages#cookies_policy", as: :cookies_policy
+  get "lgpd", to: "pages#lgpd", as: :lgpd
+  get "exclusao-de-dados", to: "pages#data_deletion", as: :data_deletion
+
   get "solucoes", to: "solutions#index", as: :solucoes
   get "funcionalidades", to: "features#index", as: :funcionalidades
   get "resultado", to: "results#index", as: :resultado
